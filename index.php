11cebc4045413b9b6949516af24095b96d0442c1
Ana Tovalin <astrostar98@gmail.com>
2014-11-12 14:56:13 -0800

    Commit after beginner milestone 4

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        // put your code here
+            echo "The Web Server is working";
         ?>
     </body>
 </html>
