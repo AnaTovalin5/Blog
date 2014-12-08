018eabca4772091807a9feb9da2013841e0f447a
Ana Tovalin <astrostar98@gmail.com>
2014-12-08 14:53:42 -0800

    Created navigation view to blog form

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
     require_once (__DIR__ . "/view/header.php");  //connects to header.php to display header code
-    require_once (__DIR__ . '/controller/create-db.php');   //connects to create-db.php file     
+    require_once (__DIR__ . "/view/navigation.php"); //connects to navigation.php
+    require_once (__DIR__ . "/controller/create-db.php");   //connects to create-db.php file     
     require_once (__DIR__ . "/view/footer.php");  //connects to footer.php to display footer code
 ?>
---
+++ view/navigation.php
@@ -0,0 +1,9 @@
+<?php
+    require (__DIR__ . "/../model/config.php");  //connects to config file
+?>
+<!--Displays link to blog post form -->
+<nav>
+    <ul>
+        <li><a href="<?php echo $path . "post.php"?>">Blog Post Form</a></li>
+    </ul>
+</nav>
